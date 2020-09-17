b9a2e466bc319d6019e48ebf702d8000d0ad7ab4
Dominic Caterina <43768546+DominicCaterina@users.noreply.github.com>
2020-09-17 15:10:39 -0400

    Tes with unity

---
+++ TestUnityProjectGit/Assets/SetRTPCToDisctance.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetRTPCToDisctance : MonoBehaviour
+{
+   public AK.Wwise.RTPC GameParameter;
+   public Transform OtherTransform;
+
+    void Update()
+    {
+       float distance = Vector3.Distance(OtherTransform.position, transform.position);
+        GameParameter.SetGlobalValue(distance);
+
+    }
+
+
+
+}
---
+++ TestUnityProjectGit/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,51 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID COLLISION = 1792046091U;
+        static const AkUniqueID DEATH_FALL = 936822103U;
+        static const AkUniqueID GOAL = 567967104U;
+        static const AkUniqueID LEVEL_MUSIC = 1244594577U;
+        static const AkUniqueID LEVEL_MUSIC_END = 4147014035U;
+        static const AkUniqueID OBSTACLE_PASS = 4251110498U;
+        static const AkUniqueID PLAYERMOVEMENT = 1350496757U;
+        static const AkUniqueID PLAYERMOVEMENT_END = 1147489399U;
+        static const AkUniqueID WIN_MUSIC = 3475929897U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID ENDLEVEL = 1054659462U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID TESTGAME = 212514547U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
